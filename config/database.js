58f9fd821a2aee449aaea3ef54bc477fd9aa487f
Patrick Jamieson <patrick@cleancodecorp.com>
2020-07-02 14:57:38 -0700

    Adds Project 2020 Players and Cards

--- config/database.js
+++ config/database.js
@@ -1,3 +1,25 @@
+
+module.exports = ({ env }) => ({
+  defaultConnection: 'default',
+  connections: {
+    default: {
+      connector: 'bookshelf',
+      settings: {
+        client: 'postgres',
+        host: env('DATABASE_HOST', 'strapi-database.cpkn13tatvlb.us-east-2.rds.amazonaws.com'),
+        port: env.int('DATABASE_PORT', 5432),
+        database: env('DATABASE_NAME', 'strapi'),
+        username: env('DATABASE_USERNAME', ''),
+        password: env('DATABASE_PASSWORD', ''),
+      },
+      options: {
+        ssl: false,
+      },
+    },
+  },
+});
+
+/*
 module.exports = ({ env }) => ({
   defaultConnection: 'default',
   connections: {
@@ -13,3 +35,4 @@ module.exports = ({ env }) => ({
     },
   },
 });
+*/
