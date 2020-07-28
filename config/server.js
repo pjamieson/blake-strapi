603cdcf9d76af6b61d5fc52f12f3508df3aa4d21
Patrick Jamieson <patrick@cleancodecorp.com>
2020-07-28 16:22:05 -0700

    Updates Strapi to version 3.1.1

--- config/server.js
+++ config/server.js
@@ -2,4 +2,9 @@ module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
   url: 'https://api.blake.art',
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET'),
+    },
+  },
 });
