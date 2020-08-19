74f8f4ae247ed6647b2532d773834e32bcc53bee
Patrick Jamieson <patrick@cleancodecorp.com>
2020-08-18 17:57:41 -0700

    Adds strapi-provider-email-gmail-2lo

---
+++ config/plugins.js
@@ -0,0 +1,14 @@
+module.exports = ({ env }) => ({
+  email: {
+    provider: 'gmail-2lo',
+    providerOptions: {
+      username: 'blake@blake.art',
+      clientId: env('GSUITE_EMAIL_CLIENT_ID'),
+      privateKey: env('GSUITE_EMAIL_PRIVATE_KEY'),
+    },
+    settings: {
+      defaultFrom: 'blake@blake.art',
+      defaultReplyTo: 'blake@blake.art',
+    },
+  },
+});
