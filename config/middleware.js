d1fe7b8f2ecb72da54f1996a413c6ec370c8ed3f
Patrick Jamieson <patrick@cleancodecorp.com>
2020-08-21 14:35:14 -0700

    Adds CORS congig

--- config/middleware.js
+++ config/middleware.js
@@ -2,7 +2,34 @@ module.exports = {
   settings: {
     cors: {
       enabled: true,
-      origin: ['http://localhost:8000', 'https://blake.art', 'https://www.blake.art'],
+      origin: [
+        'http://localhost:8000',
+        'https://blake.art',
+        'https://www.blake.art'
+      ],
+      expose: [
+        'WWW-Authenticate',
+        'Server-Authorization',
+        'Access-Control-Expose-Headers'
+      ],
+      maxAge: 31536000,
+      credentials: true,
+      methods: [
+        'GET',
+        'POST',
+        'PUT',
+        'PATCH',
+        'DELETE',
+        'OPTIONS',
+        'HEAD'
+      ],
+      headers: [
+        'Content-Type',
+        'Authorization',
+        'X-Frame-Options',
+        'Origin',
+        'Access-Control-Allow-Headers'
+      ]
     },
   },
 };
