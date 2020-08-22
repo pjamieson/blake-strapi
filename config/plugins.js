5042f13a92c90847720bd5c99ed6cb6feb92f031
Patrick Jamieson <patrick@cleancodecorp.com>
2020-08-21 18:26:17 -0700

    Update plugins.js

--- config/plugins.js
+++ config/plugins.js
@@ -4,7 +4,7 @@ module.exports = ({ env }) => ({
     providerOptions: {
       username: 'blake@blake.art',
       clientId: env('GSUITE_EMAIL_CLIENT_ID'),
-      privateKey: env('GSUITE_EMAIL_PRIVATE_KEY').replace(/\\n/g, '\n'),
+      privateKey: `${env('GSUITE_EMAIL_PK1')}${env('GSUITE_EMAIL_PK2')}${env('GSUITE_EMAIL_PK3')}`.replace(/\\n/g, '\n'),
     },
     settings: {
       defaultFrom: 'contact@blake.art',
