9aa5e1348e2fbf994cd554890ec8647b9e397aa6
Patrick Jamieson <patrick@cleancodecorp.com>
2021-02-26 21:14:11 -0800

    Adds AWS S3 image storage

--- config/plugins.js
+++ config/plugins.js
@@ -11,4 +11,15 @@ module.exports = ({ env }) => ({
       defaultReplyTo: 'contact@blake.art',
     },
   },
+  upload: {
+    provider: 'aws-s3',
+    providerOptions: {
+      accessKeyId: env('AWS_ACCESS_KEY_ID'),
+      secretAccessKey: env('AWS_ACCESS_SECRET'),
+      region: env('AWS_REGION'),
+      params: {
+        Bucket: env('AWS_BUCKET'),
+      },
+    },
+  },
 });
